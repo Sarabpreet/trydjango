6c52943ce54db254a14af3c19af9b4ddffb625d2
Sarabpreet Singh <Sarabpreetsingh23@gmail.com>
2015-10-24 18:02:12 +0530

    code++

--- src/feeds/urls.py
+++ src/feeds/urls.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    url(r'^home/', 'feeds.views.home', name='home'),
-    url(r'^resume/', 'feeds.views.resume', name='resume'),
-    # url(r'/', 'feeds.views.home1', name='home1'),
-    
+
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
